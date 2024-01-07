ffd4b3f71653ceb4d3a8d7508a3d4594609ca5b7
Walid KROUCHI <117538373+walid0912@users.noreply.github.com>
2024-01-07 13:24:53 +0100

    Update RFM_Analysis.py

--- RFM_Analysis.py
+++ RFM_Analysis.py
@@ -2,5 +2,10 @@ import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
+
 pio.templates.default = "plotly_white"
+
+data = pd.read_csv("rfm_data.csv")
+print(data.head())
+
 
